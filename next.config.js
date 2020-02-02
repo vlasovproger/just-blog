412280acde2bcfc688051acc0f26994be204ee0a
Andrei <vlasovproger@gmail.com>
2020-02-02 13:13:37 +0300

    config fix

--- next.config.js
+++ next.config.js
@@ -6,9 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const nextConfig = {
   target: "serverless",
-  transformManifest: manifest => ["/"].concat(manifest), // add the homepage to the cache
-  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-  // turn on the SW in dev mode so that we can actually test it
+  transformManifest: manifest => ["/"].concat(manifest),
   generateInDevMode: false,
   dontAutoRegisterSw: true,
   workboxOpts: {
@@ -22,7 +20,7 @@ const nextConfig = {
           networkTimeoutSeconds: 15,
           expiration: {
             maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+            maxAgeSeconds: 30 * 24 * 60 * 60
           },
           cacheableResponse: {
             statuses: [0, 200]
