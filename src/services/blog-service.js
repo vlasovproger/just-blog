b949202f93210a6164e8b24aaf68880a20516df3
vlasovproger <vlasovproger@gmail.com>
2020-02-27 16:10:22 +0300

    DB fix

--- src/services/blog-service.js
+++ src/services/blog-service.js
@@ -3,11 +3,11 @@ import firebase from "../lib/db";
 export default class ContentService {
   getArticles = async (step = 1) => {
     const articlesRef = firebase
-        .database()
-        .ref("articles/articles")
-        .orderByKey()
-        .startAt(`${step * 6}`)
-        .limitToFirst(6);
+      .database()
+      .ref("articles/articles")
+      .orderByKey()
+      .startAt(`${step * 6}`)
+      .limitToFirst(6);
     let result = await new Promise((resolve, reject) => {
       articlesRef.on("value", snapshot => {
         resolve(snapshot);
@@ -19,10 +19,10 @@ export default class ContentService {
 
   getArticle = async id => {
     const articlesRef = firebase
-        .database()
-        .ref("articles/articles")
-        .orderByChild("id")
-        .equalTo(id);
+      .database()
+      .ref("articles/articles")
+      .orderByChild("id")
+      .equalTo(id);
     let result = await new Promise((resolve, reject) => {
       articlesRef.on("value", snapshot => {
         resolve(snapshot);
